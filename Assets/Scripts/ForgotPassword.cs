4f7d856e8a182aca96baf888c59763dff63968fa
SebastianA66 <42424010+SebastianA66@users.noreply.github.com>
2019-03-08 16:09:19 +1100

    Initial commit

---
+++ Assets/Scripts/ForgotPassword.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#region Sending Emails
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+#endregion
+
+namespace sql
+{
+    public class ForgotPassword : MonoBehaviour
+    {
+
+        public string code;
+        public InputField email;
+        public string username;
+        public static System.Random random = new System.Random();
+        public void SendEmail()
+        {
+            code = RandomString(12);
+
+            MailMessage mail = new MailMessage();
+            MailAddress ourEmail = new MailAddress("sqlunityclasssydney@gmail.com", "SQueaLbusiness plz work");
+            // Receiver
+            mail.To.Add(email.text);
+            // Sender
+            mail.From = ourEmail;
+            // Topic
+            mail.Subject = "SQueaL Code Request plz work";
+            //  Main
+            mail.Body = "Hello There" + username + "\nReset your account using this code: " + code;
+
+            SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+            smtpServer.Port = 25;
+            smtpServer.Credentials = new System.Net.NetworkCredential("sqlunityclasssydney@gmail.com", "sqlpassword") as ICredentialsByHost;
+            smtpServer.EnableSsl = true;
+            ServicePointManager.ServerCertificateValidationCallback = delegate (object s, X509Certificate cert, X509Chain chain, SslPolicyErrors policyErrors) { return true; };
+            smtpServer.Send(mail);
+            Debug.Log("Succes");
+        }
+
+        public static string RandomString(int length)
+        {
+            const string chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace sql
+{
+
+    public class Login : MonoBehaviour
+    {
+        [Header("Create User Fields")]
+        public InputField create_username;
+        public InputField email, password, reEnterPassword;
+
+        [Header("Log In Fields")]
+        public InputField login_username;
+        public InputField login_password;
+
+
+        public Text notification;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (notification.text != "" && !notification.enabled)
+            {
+                notification.enabled = true;
+            }
+            if (notification.text == "" && notification.enabled)
+            {
+                notification.enabled = false;
+            }
+        }
+
+        public void CreateUserButton()
+        {
+            if (password.text == reEnterPassword.text)
+            {
+                notification.text = "";
+                StartCoroutine(CreateUser(create_username.text, email.text, password.text));
+            }
+            else
+            {
+                Debug.Log("PASSWORDS NOT THE SAME SCRUB!!!");
+            }
+        }
+
+        IEnumerator CreateUser(string _username, string _email, string _password)
+        {
+            // PHP link
+            string createUserURL = "http://localhost/squealsystem/InsertUser.php";
+
+            // Info to send to the POST variable in PHP
+            WWWForm insertUserForm = new WWWForm();
+            insertUserForm.AddField("usernamePost", _username);
+            insertUserForm.AddField("emailPost", _email);
+            insertUserForm.AddField("passwordPost", _password);
+
+            WWW www = new WWW(createUserURL, insertUserForm);
+
+            yield return www;
+            if (www.text == "User Already ExistsEmail already exists")
+            {
+                notification.text = "Account Exists";
+            }
+            else
+            {
+                notification.text = www.text;
+            }
+            Debug.Log(www.text);
+        }
+    }
+}
